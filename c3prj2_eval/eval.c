2003785a31bf00d838cafbabd8dcff0ee1eb7584
Coursera Learner <nobody@nowhere.nul>
2019-12-23 19:28:19 +0000

    changed card comp in eval

--- c3prj2_eval/eval.c
+++ c3prj2_eval/eval.c
@@ -8,11 +8,10 @@ int card_ptr_comp(const void * vp1, const void * vp2) {
   const card_t * const * cp2=vp2;
   if ((**cp1).value > (**cp2).value) return -1;
   else if ((**cp1).value < (**cp2).value) return 1;
-  else if ((**cp1).suit < (**cp2).suit) return -1;
-  else if ((**cp1).suit > (**cp2).suit) return 1;
+  else if ((**cp1).suit > (**cp2).suit) return -1;
+  else if ((**cp1).suit < (**cp2).suit) return 1;
   else return 0;
 
-  return 0;
 }
 
 suit_t flush_suit(deck_t * hand) {
